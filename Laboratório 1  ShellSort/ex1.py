bfb77d2f500e99f86225c78f9c1520706e46df32
joaoo-vitor <joao.vitor@meta.com.br>
2024-10-13 22:24:04 -0300

    separação por arquivos

---
+++ Laboratório 1  ShellSort/ex1.py
@@ -0,0 +1,14 @@
+from shellsort import *
+
+with open('Laboratório 1  ShellSort/entrada1.txt', 'r') as f:
+    for line in f:
+        arr = line.split()
+        arr = list(map(lambda x: int(x), arr))
+        n = arr.pop(0)
+        for sequence in ['SHELL', 'KNUTH', 'CIURA']:
+            shell_sort(arr, n, sequence)
+
+
+
+  
+
--- Laboratório 1  ShellSort/shellsort.py
+++ Laboratório 1  ShellSort/shellsort.py
@@ -4,7 +4,7 @@ shell_sequence = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192,
 knuth_sequence = [1, 4, 13, 40, 121, 364, 1093, 3280, 9841, 29524, 88573, 265720, 797161, 2391484]
 ciura_sequence = [1, 4, 10, 23, 57, 132, 301, 701, 1577, 3548, 7983, 17961, 40412, 90927, 204585, 460316, 1035711]
 
-def shell_sort(arr, seq_type):
+def shell_sort(arr, n, seq_type):
     # descresce começando no index np até 0 na sequencia dada
     if(seq_type == 'SHELL'):
         seq = shell_sequence
@@ -15,19 +15,18 @@ def shell_sort(arr, seq_type):
 
     # escolhe qual elemento da sequencia comaçar, partindo do ultimo e indo para a esquerda
     index_sequence = len(seq)-1
-    while(seq[index_sequence] > len(arr)):
+    while(seq[index_sequence] > n):
         index_sequence -= 1
 
     print(*arr, f'SEQ={seq_type}') 
     for i in range(index_sequence, -1, -1):
         h = seq[i]
         for j in range(0, h):
-            insDiretaShellSort(arr, h, j)
+            insDiretaShellSort(arr, n, h, j)
         print(*arr, f'INCR={h}') 
 
 
-def insDiretaShellSort(arr, h, f):
-    n = len(arr)
+def insDiretaShellSort(arr, n, h, f):
     # começa do segundo elemento do segmento até o ultimo
     for j in range(h+f, n, h):
         chave = arr[j]
@@ -39,17 +38,4 @@ def insDiretaShellSort(arr, h, f):
         arr[i+h] = chave
 
 
-# array = np.random.randint(0, 100000, size=10)
-# shell_sort(array, 'SHELL')
-
-
-
-
-with open('Laboratório 1  ShellSort/entrada1.txt', 'r') as f:
-    for line in f:
-        arr = line.split()
-        arr = list(map(lambda x: int(x), arr))
-        for sequence in ['SHELL', 'KNUTH', 'CIURA']:
-            shell_sort(arr, sequence)
-  
 
